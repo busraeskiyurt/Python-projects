8603d9b184092256623b68d57ded1f38a9745015
büşra <74591664+busraeskiyurt@users.noreply.github.com>
2020-11-20 10:19:23 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,30 +5,30 @@ def topla():
     sayi1 = int(input(" Sayı1: "))
     sayi2 = int(input(" Sayı2: ")) 
     print( sayi1 + sayi2 )
-
+# çıkarma fonksiyonu
 def cikar():
      sayi1 = int(input(" Sayı1: "))
      sayi2 = int(input(" Sayı2: "))
      print( sayi1 - sayi2) 
-
+# çarpma fonksiyonu
 def carp():
      sayi1 = int(input(" Sayı1: "))
      sayi2 = int(input(" Sayı2: ")) 
      print (sayi1 * sayi2 )
-
+# bölme fonksiyonu
 def bol():
      sayi1 = int(input(" Sayı1: "))
      sayi2 = int(input(" Sayı2: "))
  
      print (sayi1 / sayi2)
-
+# üs alma fonksiyonu
 def us():
      sayi1 = int(input(" Sayı1: "))
      sayi2 = int(input(" Sayı2: "))
  
      print (sayi1 ** sayi2) 
 
-
+# kök alma fonksiyonu
 def kok():
      sayi1 = int(input(" Sayı1: "))
  
@@ -43,7 +43,7 @@ print("1.Toplama,  2.Çıkarma, 3.çarpma,  4.Bölme, 5.üsalma,  6.kökal ")
 
 secim = input("Seçiminiz:):")
 
-
+# sayılara tıknadıında hangi fonksiyonu çalıştırmaları gerektiğini söylüyor.
    
 if secim == '1':
    topla()

6443d89a9cb32cde4404ffa3afb2bad06f5e7c6b
büşra <74591664+busraeskiyurt@users.noreply.github.com>
2020-11-20 10:16:49 +0300

    Update inverse number guessing game.py

--- inverse number guessing game.py
+++ inverse number guessing game.py
@@ -1,19 +1,15 @@
-#
-#
-#
+# bilgisayar bir sayı seçiyor ve aklımıza tuttuğumuz sayıyı bulmaya çalışıyor biz de  biglisayar sayıyı bulana kadar ona bulması için yardımcı oluyoruz.
 
-
-import random
-üstlimit=50
+import random           # andom kütüphanesini yüklüyor.
+üstlimit=50       
+altlimit=1
 print(------------------------------)
 print(      SAYI TAHMiN OYUNU       )
 print(------------------------------)
-
-altlimit=1
 print("-eger dogru bildiysem d tusuna sayiyi azaltmam gerekiyorsa a tusuna arttirmam gerekiyorsa t tusuna bas-")
-tahminsayi= random.randrange(altlimit,üstlimit)
-print("benim tahminim:  ", tahminsayi)
-y =input("dogrumu")
+tahminsayi= random.randrange(altlimit,üstlimit)               #üstlimit ve altlimit sayıları arasında bir sayı seçiyor.
+print("benim tahminim:  ", tahminsayi)                  # benim tahminim deyip tahminini ekrana yazdırıyor
+y =input("dogrumu")                          #tahmini doğru mu diye souyor. ve bunu y değişkenine atıyor
 d = ("d")
 a = ("a")
 t = ("t")
@@ -21,28 +17,28 @@ t = ("t")
 
 while True:
 
-    if y=="q" or y=="Q":
+    if y=="q" or y=="Q":                    #cevaba eğer q veya Q girildiye oyundan çıkıyor.
         print("çikis yaptiniz.")
         break
 
 
-    elif y == "d"  :
+    elif y == "d"  :                         # vevap d ise tebrikler deyip oyunu bitiiyor.
         print("tebrikler sayiyi dogru bildin..")
         break
         
-    elif y == "a" :
+    elif y == "a" :                         # cevap eğer a ise stlimitin değernn tahmin sayıdan bir azaltıyor çünk verdii cevaptan daha az bir ayı vermei gerek
                     
         üstlimit= (tahminsayi - 1)
-        tahminsayi= random.randrange(altlimit,üstlimit)
+        tahminsayi= random.randrange(altlimit,üstlimit)    # tkrardan tahmin yapıyor ve ekrana yazdırıp doru mu diye sooruyor.
         print(tahminsayi)
         y =input("dogru mu?")
                                 
-                    
-    elif y == "t" :
+               
+    elif y == "t" :                                     # ceva eğer t ise altlimitin değernn tahmin saıdan bie arttırıyor. çünk verdii cevaptan daha büyük bir sayı vermei gerek  
         altlimit= (tahminsayi + 1)
         tahminsayi= random.randrange(altlimit,üstlimit)
-        print(tahminsayi)
+        print(tahminsayi)                                        # tkrardan tahmin yapıyor ve ekrana yazdırıp doru mu diye sooruyor.
         y =input("dogru mu?")
     else :
-        print("lütfen a, d veya t tusuna basin")
+        print("lütfen a, d, q veya t tusuna basin")             # eğer başka bir tua basarsa uyarı veriyor.
                    
